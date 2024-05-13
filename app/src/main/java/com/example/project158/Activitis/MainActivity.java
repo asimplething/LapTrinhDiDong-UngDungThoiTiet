dfb2e0b4b56602cf7909efe162d4ba23fe17b498
Tiendvt <dovantientk@gmail.com>
2024-05-13 20:27:51 +0700

    fix layout
    fix future activity

--- app/src/main/java/com/example/project158/Activitis/MainActivity.java
+++ app/src/main/java/com/example/project158/Activitis/MainActivity.java
@@ -1,21 +1,19 @@
 package com.example.project158.Activitis;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.example.project158.API.APIService;
