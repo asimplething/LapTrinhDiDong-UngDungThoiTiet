5b451c95a9af9b2217aec463a1e1820abd364a0f
Vinh2403 <phamv4356@gmail.com>
2024-05-13 15:45:39 +0700

    Quang Vinh mới thêm các class như "Current","ResponseWrapper", và chỉnh sửa thêm việc gọi API load dữ liệu thời tiết hiện tại lên MainActivity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,5 +37,11 @@ dependencies {
     implementation("com.google.android.material:material:1.8.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.github.bumptech.glide:glide:4.12.0")
+    //load ảnh với Glide
+    implementation ("com.github.bumptech.glide:glide:4.14.2")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.14.2")
 
+//Gson và Volley:
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
 }
